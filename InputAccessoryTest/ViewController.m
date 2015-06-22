aa1215666022798af7198038976aee925b9691db
Christian Schnorr <christian.schnorr@me.com>
2015-06-22 13:06:51 +0200

    Use text view as input accessory view of controller

--- InputAccessoryTest/ViewController.m
+++ InputAccessoryTest/ViewController.m
@@ -8,63 +8,48 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
 
+@property (nonatomic, strong) UIView *inputAccessoryView;
 @end
 
-@implementation ViewController{
-    NSArray *constraintsOfTextInputView;
-    UITextView *editingTextView;
+@implementation ViewController
+
+- (void)loadView
+{
+    UIScrollView *const scrollView = [[UIScrollView alloc] initWithFrame:UIScreen.mainScreen.applicationFrame];
+    scrollView.alwaysBounceVertical = YES;
+    scrollView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
+    scrollView.backgroundColor = UIColor.whiteColor;
+
+    self.view = scrollView;
 }
 
-- (IBAction)dismissTapped:(id)sender {
-    [editingTextView resignFirstResponder];
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    [UIView performWithoutAnimation:^{
+        [self becomeFirstResponder];
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
--(void)viewDidLoad{
-    [super viewDidLoad];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide) name:UIKeyboardWillHideNotification object:nil];
-}
-
--(NSArray*)constraintsRelatedToView:(UIView*)view{
-    NSMutableArray *array = [NSMutableArray array];
-    for (NSLayoutConstraint *constraint in self.view.constraints) {
-        if(constraint.firstItem == view || constraint.secondItem == view){
-            [array addObject:constraint];
-        }
-    }
-    return array;
-}
-
--(BOOL)textViewShouldBeginEditing:(UITextView *)textView{
-    UIView *inputView = self.textInputView; //this is the main view that we are adding to top of keyboard.
-    constraintsOfTextInputView = [self constraintsRelatedToView:inputView]; //get all constraints of that view and store.
-    [inputView removeFromSuperview]; //not really needed, just convenience for running when next line is commented out
-    [[UIApplication sharedApplication].keyWindow addSubview:self.textInputView]; //adds that view to window (if I don't call it it doesn't get added)
-    textView.inputAccessoryView = inputView; //set it as the input accessory view for the active text view
-    editingTextView = textView; //store a ref to editing text view
+- (BOOL)canBecomeFirstResponder
+{
     return YES;
 }
 
--(void)keyboardWillHide{
-    if(editingTextView && constraintsOfTextInputView){ //if we are editing
-        editingTextView.inputAccessoryView = nil; //set input accessory view to nil
-        [self.textInputView removeFromSuperview]; //remove our accessory view from whatever container it was in (I confirm it gets nil here)
-        
-        // THE NEXT LINE THROWS EXCEPTION !!!
-        
-        [self.view addSubview:self.textInputView]; //add our view back to view controller's hiearchy, effectively undo-ing what we've done initially.
-        [self.view addConstraints:constraintsOfTextInputView]; //add the constraints again
-        [self.view layoutIfNeeded];
-        
-        editingTextView = nil;
-        constraintsOfTextInputView = nil;
+- (UIView *)inputAccessoryView
+{
+    if (_inputAccessoryView == nil) {
+        _inputAccessoryView = ({
+            UITextView *const textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 320, 50)];
+            textView.backgroundColor = UIColor.redColor;
+            textView;
+        });
     }
+
+    return _inputAccessoryView;
 }
 
 @end
